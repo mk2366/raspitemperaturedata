64ecdba2b6a480fcbc01e72562b4f54132c437d7
Markus Kupke <kupkemarkus@gmail.com>
2018-01-06 18:30:29 +0100

    read list of sensors

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # raspitemperaturedata
 
+__Don't read on. This project is in beginning phase!__
+
 Small python project to collect temperature data via a DS18B20 connected to a Raspberry Pi.
 The data will be collected on a server using [MariaDB](http://mariadb.org/).
--- temperature_data_sensor.py
+++ temperature_data_sensor.py
@@ -1,10 +1,32 @@
 import MySQLdb
 import os
+import glob
+import logging
 
+# read environment to connect to a remote mariaDB
 __host__ = os.environ['DB_HOST']
 __user__ = os.environ['DB_USER']
 __passwd__ = os.environ['DB_PASSWD']
 __db__ = os.environ['DB_DB']
+
+__sensors__ = []
+
+# determine the connected sensors and store the names into __sensors__ list
+try:
+    # constants taken from
+    # https://github.com/spotify/linux/blob/master/drivers/w1/w1_family.h
+    __sensors__ = (glob.glob('/sys/bus/w1/devices/28-*') +
+                   glob.glob('/sys/bus/w1/devices/10-*') +
+                   glob.glob('/sys/bus/w1/devices/22-*'))
+except:
+    logging.error("Couldn't find any sensor in /sys/bus/w1/devices")
+    raise
+if len(__sensors__) == 0:
+    logging.error("Couldn't find any sensor in /sys/bus/w1/devices")
+    raise RuntimeError("Couldn't find any sensor in /sys/bus/w1/devices")
+
+print __sensors__
+exit()
 
 
 try:
@@ -22,6 +44,3 @@ with mariaDB.cursor() as cur:
 
 mariaDB.commit()
 mariaDB.close()
-    
-
-
