d6d12109846bca5335791ea688ce53fb39d0d190
Markus Kupke <kupkemarkus@gmail.com>
2018-02-05 22:11:20 +0100

    Environment variables

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 me=`realpath $0`
 base=`dirname $me`
 dat=`date`
 
 cd $base
+echo `pwd`
 mv nohup.out "nohup.out$dat"
 nohup pipenv run ./temperature_data_sensor.py &
